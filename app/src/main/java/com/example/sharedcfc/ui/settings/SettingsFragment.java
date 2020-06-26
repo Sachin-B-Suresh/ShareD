1305d95938a29a974236f733859acce1e51e2db4
Sachin <sachin12199@gmail.com>
2020-06-26 17:36:55 +0530

    Updated SettingsFragment

--- app/src/main/java/com/example/sharedcfc/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/sharedcfc/ui/settings/SettingsFragment.java
@@ -1,15 +1,26 @@
 package com.example.sharedcfc.ui.settings;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 
 import com.example.sharedcfc.R;
 
 import androidx.annotation.Nullable;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+    Preference notificationsPref;
+    Preference locationPref;
+    Context context;
 
     @SuppressWarnings("deprecation")
     @SuppressLint("ResourceType")
@@ -22,6 +33,62 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         // TODO: Use the ViewModel
+        notificationsPref = (Preference) findPreference("updateNotifications");
+        locationPref = (Preference) findPreference("updateLocation");
+        notificationsPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                //open browser or intent here
+                goToNotificationSettings();
+                return true;
+            }
+        });
+        locationPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                //open browser or intent here
+                updateLocation();
+                return true;
+            }
+        });
     }
+    public void goToNotificationSettings() {
 
+        String packageName = "com.example.sharedcfc";
+        try {
+            Intent intent = new Intent();
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+
+                intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+                intent.putExtra(Settings.EXTRA_APP_PACKAGE, packageName);
+                intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+
+            } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O) {
+
+                intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+                intent.putExtra("android.provider.extra.APP_PACKAGE", packageName);
+
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+                intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
+                intent.putExtra("app_package", packageName);
+                intent.putExtra("app_uid", context.getApplicationInfo().uid);
+
+            } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
+
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setData(Uri.parse("package:" + packageName));
+
+            } else {
+                return;
+            }
+
+            startActivity(intent);
+
+        } catch (Exception e) {
+            // log goes here
+
+        }
+    }
+    public void updateLocation(){
+    }
 }
